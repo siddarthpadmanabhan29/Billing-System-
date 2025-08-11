fdaa09e7fa15a2a2b147da7e8119aa7c11d6ef1e
siddarthpadmanabhan29 <siddarthrp15@hotmail.com>
2025-08-10 23:08:21 -0400

    Update billing.sql

--- billing.sql
+++ billing.sql
@@ -35,20 +35,20 @@ CREATE TABLE IF NOT EXISTS CreditCards (
 
 -- Including Sample Data of Each Statement
 -- Items Sample for SQL Insertion
-INSERT INTO Items (name, quantity, price) VALUES (Pizza, 2, 14.99);
-INSERT INTO Items (name, quantity, price) VALUES (Pillows, 5, 35.17);
+INSERT INTO Items (name, quantity, price) VALUES ('Pizza', 2, 14.99);
+INSERT INTO Items (name, quantity, price) VALUES ('Pillows', 5, 35.17);
 
 -- Bills Sample for SQL Insertion
 INSERT INTO Bills (total, tax) VALUES (256.77, 12.84);
 INSERT INTO Bills (total, tax) VALUES (8367.99, 418.40);
 
 -- Customers Sample for SQL Insertion
-INSERT INTO Customers (name, email, phone) VALUES (Dave Johnson, davejohnson@gmail.com, 617-234-1234);
-INSERT INTO Customers (name, email, phone) VALUES (Camila Neal, camilaneal@hotmail.com, 555-653-5678);
+INSERT INTO Customers (name, email, phone) VALUES ('Dave Johnson', 'davejohnson@gmail.com', '617-234-1234');
+INSERT INTO Customers (name, email, phone) VALUES ('Camila Neal', 'camilaneal@hotmail.com', '555-653-5678');
 
 -- Credit Cards Smaple for SQL Insertion
-INSERT INTO CreditCards (customer_id, cardNumber, expiration) VALUES (1, 74938472349, 05/10);
-INSERT INTO CreditCards (customer_id, cardNumber, expiration) VALUES (2, 21343214321, 12/25);
+INSERT INTO CreditCards (customer_id, cardNumber, expiration) VALUES (1, '74938472349', '05/10');
+INSERT INTO CreditCards (customer_id, cardNumber, expiration) VALUES (2, '21343214321', '12/25');
 
 
 
